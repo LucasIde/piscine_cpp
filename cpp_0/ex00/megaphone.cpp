ac7e75a57926426ae125c46fc45396965246cc07
LucasIde <lucaside000@gmail.com>
2022-09-20 19:57:59 +0200

    first push

---
+++ cpp_0/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lide <lide@student.s19.be>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 18:04:47 by lide              #+#    #+#             */
+/*   Updated: 2022/09/20 18:57:35 by lide             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv) {
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+			{
+				if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+					std::cout << (char)(argv[i][j] - 32);
+				else
+					std::cout << argv[i][j];
+			}
+		}
+		std::cout << std::endl;
+	}
+}
