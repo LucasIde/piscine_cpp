e138f4dd1b97e9c2cb2ddb757d284fd6cc1c54ca
LucasIde <lucaside000@gmail.com>
2022-09-22 19:44:36 +0200

    phonebook presque fini

--- cpp_0/ex01/PhoneBook.cpp
+++ cpp_0/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: lide <lide@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 17:09:36 by lide              #+#    #+#             */
-/*   Updated: 2022/09/22 02:01:22 by lide             ###   ########.fr       */
+/*   Updated: 2022/09/22 19:42:28 by lide             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,23 @@ PhoneBook::PhoneBook(void) : index(-1) {}
 
 PhoneBook::~PhoneBook(void) {}
 
-void	find_str(std::string line, std::string *str) {
-	while ((*str)[0] == 0)
+void	find_str(std::string info, std::string *str) {
+	std::string line;
+	while (line[0] == 0)
 	{
-		std::cout << line;
-		std::cin >> *str;
-		if ((*str)[0] == 0)
+		std::cout << info;
+		std::getline(std::cin, line);
+		if (line[0] == 0)
 			std::cout << "need some information !" << std::endl;
 	}
+	*str = line;
 }
 
-//doit gerer + de 8
 void	PhoneBook::add(void) {
 	this->index++;
-	if (this->index == 8)
+	if (this->index > 7)
 		this->index = 0;
 	find_str("first name : ", &(this->contact[index].first_name));
-	std::cout << this->contact[index].first_name << " test " << std::endl;
 	find_str("last name : ", &(this->contact[index].last_name));
 	find_str("nickname : ", &(this->contact[index].nickname));
 	find_str("phone number : ", &(this->contact[index].number));
@@ -48,8 +48,8 @@ std::string	ft_short(std::string name) {
 			shorted.insert(0, " ");
 	else if (len > 10)
 	{
-		shorted.resize(9);
-		shorted + '.';
+		shorted.resize(10);
+		shorted[9] = '.';
 	}
 	return (shorted);
 }
@@ -63,16 +63,21 @@ void	PhoneBook::search(void) const {
 		std::cout << "there is no contact in the PhoneBook" << std::endl;
 		return ;
 	}
-	for (int i = 0; this->contact[i].first_name[0] != 0 && i < 8; i++)
+	for (int i = 0;i < 8 && this->contact[i].first_name[0] != 0; i++)
 		std::cout << "|         " << i
 		<< "|" << ft_short(this->contact[i].first_name)
 		<< "|" << ft_short(this->contact[i].last_name)
 		<< "|" << ft_short(this->contact[i].nickname) << "|" << std::endl;
 	std::cout << "Enter the contact ID : ";
-	std::cin >> id;
-	if (id.length() > 1 || (id[0] < '0' && id[0] > '7') || this->contact[atoi(id.c_str())].first_name[0] == 0)//segfault
+	std::getline(std::cin, id);
+	if (id.length() > 1 || id[0] < '0' || id[0] > '7')
 	{
 		std::cout << "Wrong ID" << std::endl;
+		return ;
+	}
+	else if (this->contact[atoi(id.c_str())].first_name[0] == 0)
+	{
+		std::cout << "there is no contact saved for this ID" << std::endl;
 		return ;
 	}
 	else
--- cpp_0/ex01/main.cpp
+++ cpp_0/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lide <lide@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/22 01:39:26 by lide              #+#    #+#             */
-/*   Updated: 2022/09/22 02:03:42 by lide             ###   ########.fr       */
+/*   Updated: 2022/09/22 19:27:01 by lide             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ int main(void)
 	std::string line;
 	PhoneBook phone;
 
-	while (1)
+	while ((line.compare("EXIT")))
 	{
-		std::cout << "ADD | SEARCH" << std::endl;
-		std::cin >> line;
+		std::cout << "The program only accepts ADD, SEARCH and EXIT :" << std::endl;
+		std::getline(std::cin, line);
 		if (!line.compare("ADD"))
 			phone.add();
 		else if (!line.compare("SEARCH"))
