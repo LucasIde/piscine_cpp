10499fa95c6da9cd66293f27573eb4ac8701a563
LucasIde <lucaside000@gmail.com>
2022-11-28 16:41:36 +0100

    plus rapide

--- cpp_1/ex01/main.cpp
+++ cpp_1/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lide <lide@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 18:17:38 by lide              #+#    #+#             */
-/*   Updated: 2022/09/27 20:15:05 by lide             ###   ########.fr       */
+/*   Updated: 2022/11/28 14:46:28 by lide             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main(void) {
 	Zombie	*horde;
-	int		nb = 10;
+	int		nb = 5;
 
 	horde = zombieHorde(nb, "Zombie");
 	for (int i = 0; i < nb; i++)
--- cpp_1/ex01/zombieHorde.cpp
+++ cpp_1/ex01/zombieHorde.cpp
@@ -6,13 +6,19 @@
 /*   By: lide <lide@student.s19.be>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 19:14:42 by lide              #+#    #+#             */
-/*   Updated: 2022/09/26 19:24:56 by lide             ###   ########.fr       */
+/*   Updated: 2022/11/28 14:45:54 by lide             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie* zombieHorde(int N, std::string name) {
+	if (N < 1)
+	{
+		std::cout << "horde can't be less than 1" << std::endl;
+		return (NULL);
+	}
+
 	Zombie *horde = new Zombie[N];
 
 	for(int i = 0; i < N; i++)
