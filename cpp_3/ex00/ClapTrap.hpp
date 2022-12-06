a6fe03b637dd001595cbc5ed6bdb501c2390b13f
LucasIde <lucaside000@gmail.com>
2022-12-06 21:43:34 +0100

    push rapide

---
+++ cpp_3/ex00/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lide <lide@student.s19.be>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/06 21:11:11 by lide              #+#    #+#             */
+/*   Updated: 2022/12/06 21:24:07 by lide             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <string>
+
+class ClapTrap {
+	private:
+		std::string	_Name;
+		int			_Hit_Points;
+		int			_Energy_points;
+		int			attack_damage;
+
+	public:
+	ClapTrap(void);
+	ClapTrap(ClapTrap const &src);
+	~ClapTrap(void);
+
+	ClapTrap	&operator=(ClapTrap const &rhs);
+
+	void attack(const std::string& target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+};
+
+#endif
